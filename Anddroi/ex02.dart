b546781b6152ced6ddd9e84ebb6a2c615983a0d9
luonggiahuy1234564 <145769771+luonggiahuy1234564@users.noreply.github.com>
2024-11-22 12:18:23 +0700

    Add files via upload

---
+++ Anddroi/ex02.dart
@@ -0,0 +1,9 @@
+void main(){
+  var ten="Huy";
+  String name= "Gia";
+final String x='value';
+const int nam= 2024;
+late String moTa='Gioi thieu mon hoc';
+print('Mo ta:$moTa');
+print('Nam:$nam');
+}
