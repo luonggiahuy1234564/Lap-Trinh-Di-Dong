1b2c4cff983d0260c70b35c36039afdb9138c2a2
luonggiahuy1234564 <145769771+luonggiahuy1234564@users.noreply.github.com>
2024-11-22 12:20:22 +0700

    Delete Anddroi directory

--- Anddroi/ex02.dart
+++
@@ -1,9 +0,0 @@
-void main(){
-  var ten="Huy";
-  String name= "Gia";
-final String x='value';
-const int nam= 2024;
-late String moTa='Gioi thieu mon hoc';
-print('Mo ta:$moTa');
-print('Nam:$nam');
-}
